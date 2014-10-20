224aa43ba1f65594c09045d0b2f13a41aae63e33
Shinmera <shinmera@tymoon.eu>
2014-10-20 21:00:15 +0200

    Exports

--- module.lisp
+++ module.lisp
@@ -6,5 +6,19 @@
 
 (in-package #:rad-user)
 (define-module #:keyword-reviews
-  (:use #:cl #:radiance))
+  (:use #:cl #:radiance)
+  ;; objects.lisp
+  (:export
+   #:make-type
+   #:delete-type
+   #:edit-type
+   #:make-review
+   #:delete-review
+   #:edit-review)
+  ;; toolkit.lisp
+  (:export
+   #:ensure-type
+   #:ensure-review
+   #:types
+   #:reviews))
 (in-package #:keyword-reviews)
